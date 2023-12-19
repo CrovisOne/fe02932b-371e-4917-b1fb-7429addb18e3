c6be9c898c3cb604536400326a506370116ced06
CrovisOne <alex.brjazgunov235@gmail.com>
2023-12-19 12:48:14 +0100

    dev(App): Organize Routes

---
+++ src/routes/appRoutes.ts
@@ -0,0 +1,15 @@
+export enum BASE_ROUTES {
+  Home = "/",
+  Events = "/events",
+  Locations = "/clubs",
+  Cart = "/cart",
+}
+
+export const appRoutes = {
+  home: BASE_ROUTES.Home,
+  searchEvents: BASE_ROUTES.Events,
+  event: `${BASE_ROUTES.Events}/{id}`,
+  searchLocation: `${BASE_ROUTES.Locations}`,
+  location: `${BASE_ROUTES.Locations}/{id}`,
+  cart: BASE_ROUTES.Cart,
+};
