3386eae82daf19d9e397dc0cea5ec18b1a64f26a
CrovisOne <alex.brjazgunov235@gmail.com>
2023-12-22 09:59:24 +0100

    dev: Optimize cleanedDate
    if start == end than just display start

--- src/components/cards/ImageCard/scripts/handleDate.ts
+++ src/components/cards/ImageCard/scripts/handleDate.ts
@@ -4,6 +4,8 @@ export function cleanedDate(startTime?: string, endTime?: string): string {
   let cleanStart = startTime ? trimTimeFromDate(startTime) : "";
   let cleanEnd = endTime ? trimTimeFromDate(endTime) : "";
 
+  if (cleanStart && cleanEnd && cleanStart === cleanEnd) return cleanStart;
+
   return cleanStart || cleanEnd
     ? `${cleanStart}${cleanEnd ? ` - ${cleanEnd}` : ""}`
     : "Coming soon";
